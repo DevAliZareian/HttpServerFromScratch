cb0cf7d09dde558fc2b1fe686209018a98b390c5
Ali <dev.ali.zareian@gmail.com>
2026-08-08 17:11:39 +0330

    Add initial project setup

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using HttpServer.Core;
+
+namespace HttpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "--port" && i + 1 < args.Length)
+                {
+                    if (int.TryParse(args[i + 1], out int port))
+                    {
+                        Server.Start(port);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid port number.");
+                    }
+                }
+            }
+        }
+    }
+}
