ce2ceb5938a58fe775fa267d92da64571b4dd44b
Ali <dev.ali.zareian@gmail.com>
2026-08-08 17:11:42 +0330

    Add utility helpers for command execution and path finding

---
+++ Utils/Cmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpServer.Utils
+{
+    public class Cmd
+    {
+        public static void Execute(string command)
+        {
+            System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.Arguments = command;
+            cmd.StartInfo.Verb = "runas";
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+        }
+    }
+}
---
+++ Utils/PathFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpServer.Utils
+{
+    public class PathFinder
+    {
+        // TODO: Find the file base on the path.
+    }
+}
